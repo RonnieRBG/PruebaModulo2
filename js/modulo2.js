616199850faec37aa6a6894aa2d0652c7f87fc3f
RonnieRBG <johnatan.rodriguezg@gmail.com>
2025-06-28 20:37:59 -0400

    añadiendo jquery

--- js/modulo2.js
+++ js/modulo2.js
@@ -1,3 +1,21 @@
-.texto_central {
-    
-}
+$(document).ready(function() {
+  $(".caja").hover(
+    function() {
+      $(this).addClass("hovered");
+    },
+    function() {
+      $(this).removeClass("hovered");
+    }
+  );
+});
+
+$(document).ready(function () {
+  // Oculta todos los <p> al cargar
+  $(".caja p").hide();
+
+  // Toggle al hacer clic en .caja
+  $(".caja").click(function () {
+    $(this).find("p").slideToggle(); // Alterna con animación
+  });
+});
+
